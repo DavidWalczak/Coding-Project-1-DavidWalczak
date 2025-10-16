4b20dbfdc604519debcca85f98eb41c9be3069af
David Walczak <dwalczak@usf.edu>
2025-10-15 22:53:29 -0400

    began adding ja, creating a load function first

---
+++ cp_1.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('feedbackForm');
+  const feedbackDisplay = document.getElementById('feedbackDisplay');
+  const charCount = document.getElementById('charCount');
+  const tooltip = document.createElement('div');
+  tooltip.classList.add('tooltip');
+  document.body.appendChild(tooltip);
+
+
+});
